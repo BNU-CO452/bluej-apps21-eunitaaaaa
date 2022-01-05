170f073c6191a15937e79240078a4cf3dc011f94
Eunita Da Silva <93047018+eunitaaaaa@users.noreply.github.com>
2022-01-05 14:35:42 +0000

    App212

--- app212-grades/Student.java
+++ app212-grades/Student.java
@@ -111,7 +111,7 @@ public class Student
     {
         System.out.println(" ------------------------------------");
         System.out.println(" App21-02: Exam Board Transcript 2021");
-        System.out.println("        by student name");
+        System.out.println("        by Eunita");
         System.out.println(" ------------------------------------");
         
         printCourse();
